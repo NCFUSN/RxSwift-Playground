e2db4d9fc1ae5120e0ebef2536623c75e9eb1313
Nathan Furman <ncfusn@gmail.com>
2018-01-11 14:45:15 +0200

    Files added
    Files added

---
+++ RxSwift-Playground/Model/FSTopic.swift
@@ -0,0 +1,19 @@
+///**
+
+/**
+ 
+ RxSwift-Playground
+ 
+ Created by: Nathan Furman on 1/11/18
+ Email: ncfusn@gmail.com
+ Copyright (c) 2018 Nathan Furman, All Rights Recerved
+ 
+ */
+
+import Foundation
+
+struct FSTopic {
+    
+    var title: String
+    var subtitle: String
+}
--- RxSwift-Playground/View/FSMainTableViewController.swift
+++ RxSwift-Playground/View/FSMainTableViewController.swift
@@ -1,26 +1,14 @@
 ///**
 
 /**
-
-RxSwift-Playground
-
-Created by: Nathan Furman on 1/11/18
-Email: nathan@latto.tv
-
-Latto Proprietary
-
-Copyright (c) 2018 Latto, All Rights Recerved
-
-All data and information contained in or disclosed by this document are
- confidential and proprietary information of Latto, and all rights
- therein are expressly reserved. By accepting this material, the
- recipient agrees that this material and the information contained
- therein are held in confidence and in trust. The material may only be
- used and/or disclosed as authorized in a license agreement controlling
- such use and disclosure.
-
-*/
-
+ 
+ RxSwift-Playground
+ 
+ Created by: Nathan Furman on 1/11/18
+ Email: ncfusn@gmail.com
+ Copyright (c) 2018 Nathan Furman, All Rights Recerved
+ 
+ */
 
 import UIKit
 
@@ -28,12 +16,7 @@ class FSMainTableViewController: UITableViewController {
         
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        self.title = "Topics"
     }
 
     override func didReceiveMemoryWarning() {
@@ -44,16 +27,14 @@ class FSMainTableViewController: UITableViewController {
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
         return 0
     }
 
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
 
@@ -61,51 +42,9 @@ class FSMainTableViewController: UITableViewController {
 
         return cell
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
     
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        //
+    }
+ 
 }
